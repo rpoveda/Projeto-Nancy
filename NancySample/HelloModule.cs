43f8a64be765066149ba0e8de70b40207c45e00d
unknown <rpoveda@SOROCABA9-PC.vm2sorocaba.com>
2012-05-31 13:05:04 -0300

    subindo o arquivo

---
+++ NancySample/HelloModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace NancySample
+{
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/"] = parameters => "Hello World";
+            Get["/About"] = parameters => "About test";
+        }
+    }
+}
